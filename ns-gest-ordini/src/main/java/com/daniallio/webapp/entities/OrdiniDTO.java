2b69c8f5401cf2e5b109b1af6c494a57700279e5
daniallio <daniallio@gmail.com>
2023-08-21 16:21:38 +0200

    classe dto ordini

---
+++ ns-gest-ordini/src/main/java/com/daniallio/webapp/entities/OrdiniDTO.java
@@ -0,0 +1,27 @@
+package com.daniallio.webapp.entities;
+
+import java.util.Date;
+
+import lombok.Data;
+
+@Data
+public class OrdiniDTO {
+
+	
+	private int key;
+	
+	private String descrizione;
+	
+	private double valore;
+	
+	private double ore;
+	
+	private double oreResidue;
+	
+	private double valResiduo;
+	
+	private Date dataOrd;
+	
+	private String cliente;
+	
+}
